98335baf28e36c3e0b0862950755d5cf83a100ff
EmanuelCanova <canova.9400@gmail.com>
2019-06-14 07:50:15 -0300

    Cambios en Post

--- src/components/Post.js
+++ src/components/Post.js
@@ -5,44 +5,42 @@ import Footer from './Footer'
 import './style/Post.scss';
 
 
-const Post = (props) =>{
-    
+const Post = (props) => {
+
     const pathname = (props.location.pathname).split("/");
-    
+
 
     const [data, loading] = useFetch(
         "https://next.json-generator.com/api/json/get/EyDse5L0U"
-      );
-    return(
+    );
+    return (
         <div>
-            <Header/>
+            <Header />
             <div className="clearfix"></div>
-            <div className="postContainer">
-                <div className="wave">
-                    <div className="container">
-                        
-                        <h1><i className="fas fa-chevron-right"></i> Ver mas</h1>
-                    </div>
+            <div className="redBanner">
+                <div className="postContainer container">
+                <i className="fas fa-chevron-right"></i>
+                <h1>Ver Mas</h1>
                 </div>
             </div>
             <div className="container postBody">
-                    {loading ? 
-                        (<div className="container">
-                            <div className="loader"></div>
-                        </div>)
-                        :
-                        (<>
-                            <h1>{data[0].title}</h1>
-                            <p>{data[0].content}</p>
-                        </>
-                        )
+                {loading ?
+                    (<div className="container">
+                        <div className="loader"></div>
+                    </div>)
+                    :
+                    (<>
+                        <h1>{data[0].title}</h1>
+                        <p>{data[0].content}</p>
+                    </>
+                    )
                 }
             </div>
             <section id="footer">
-                    <Footer/>
-               </section>
+                <Footer />
+            </section>
         </div>
-        
+
 
     );
 }
